cb5bd6471ec635b6585483c5ab097e8f09b41f18
Kashish Juneja <kashishjuneja1010@gmail.com>
2025-07-26 12:10:04 +0530

    Create 1407. Top Travellers.sql

---
+++ 1407. Top Travellers.sql
@@ -0,0 +1,10 @@
+SELECT name, 
+    CASE
+        WHEN SUM(distance) IS NULL THEN 0
+        ELSE SUM(distance)
+    END AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r
+ON u.id = r.user_id
+GROUP BY r.user_id
+ORDER BY travelled_distance DESC, name ASC;
